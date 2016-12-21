8691dafcd8fe8938957b427c7f2ec3f0dd921f17
swd <(311715)>
2016-12-21 11:58:26 +0800

    init

--- src/main/java/com/swd/pets/MySQLConfig.java
+++ src/main/java/com/swd/pets/MySQLConfig.java
@@ -1,6 +1,5 @@
 package com.swd.pets;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -33,50 +32,50 @@ public class MySQLConfig {
     @Value("${mysql.driver_class}")
     private String driverName;
 
-    @Bean(name = "mysqlDataSource")
-    public DataSource mysqlDataSource() {
+//    @Bean(name = "mysqlDataSource")
+//    public DataSource mysqlDataSource() {
 
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setUser(user);
-        dataSource.setJdbcUrl(url);
-        dataSource.setPassword(password);
-        setDataSourceProperty(dataSource);
-
-        return dataSource;
-    }
-
-
-    private void setDataSourceProperty(ComboPooledDataSource dataSource) {
-        dataSource.setInitialPoolSize(15);
-        dataSource.setMaxPoolSize(30);
-        dataSource.setMinPoolSize(5);
-        dataSource.setMaxIdleTime(25200);
-        dataSource.setMaxStatements(0);
-        dataSource.setIdleConnectionTestPeriod(30);
-        dataSource.setAcquireIncrement(20);
-        dataSource.setAcquireRetryAttempts(30);
-        dataSource.setPreferredTestQuery("SELECT 1");
-        dataSource.setTestConnectionOnCheckout(false);
-        dataSource.setBreakAfterAcquireFailure(true);
-    }
+//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+//        dataSource.setUser(user);
+//        dataSource.setJdbcUrl(url);
+//        dataSource.setPassword(password);
+//        setDataSourceProperty(dataSource);
+//
+//        return dataSource;
+//    }
 
 
-    @Bean
-    public NamedParameterJdbcTemplate jdbcTemplate() {
-        NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(mysqlDataSource());
-        return jdbcTemplate;
-    }
+//    private void setDataSourceProperty(ComboPooledDataSource dataSource) {
+//        dataSource.setInitialPoolSize(15);
+//        dataSource.setMaxPoolSize(30);
+//        dataSource.setMinPoolSize(5);
+//        dataSource.setMaxIdleTime(25200);
+//        dataSource.setMaxStatements(0);
+//        dataSource.setIdleConnectionTestPeriod(30);
+//        dataSource.setAcquireIncrement(20);
+//        dataSource.setAcquireRetryAttempts(30);
+//        dataSource.setPreferredTestQuery("SELECT 1");
+//        dataSource.setTestConnectionOnCheckout(false);
+//        dataSource.setBreakAfterAcquireFailure(true);
+//    }
 
-
-    @Bean
-    @Qualifier(value = "mySqlTransactionManager")
-    public DataSourceTransactionManager mySqlTransactionManager() {
-
-        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
-        dataSourceTransactionManager.setDataSource(mysqlDataSource());
-
-        return dataSourceTransactionManager;
-    }
+//
+//    @Bean
+//    public NamedParameterJdbcTemplate jdbcTemplate() {
+//        NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(mysqlDataSource());
+//        return jdbcTemplate;
+//    }
+//
+//
+//    @Bean
+//    @Qualifier(value = "mySqlTransactionManager")
+//    public DataSourceTransactionManager mySqlTransactionManager() {
+//
+//        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+//        dataSourceTransactionManager.setDataSource(mysqlDataSource());
+//
+//        return dataSourceTransactionManager;
+//    }
 
 
 }
